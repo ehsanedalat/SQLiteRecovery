1edafa871d52b0bc0ba9630c094e954caa63958b
ehsan-is-everything <ehsan.e.71@gmail.com>
2015-07-11 13:01:28 +0430

    first commit!

---
+++ DevicePluginLibrary/DevicePluginInteface.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLiteRecovery
+{
+    /// <summary>
+    ///     this is plugin interface. for each device we should create a class that inherited this interface.
+    ///     this interface has 3 methode:
+    ///         root()
+    ///             this methode should override and  impelement the procedure of accessing root access in each device.
+    ///         copyFilesFromDevice()
+    ///             this methode is used for copping sqlite DB files from device to computer.
+    ///         setAppCheckBoxes()
+    ///             this methode is used for producing checkboxes that should add to main tab of device.
+    /// </summary>
+    interface DeviceRecoveryPluginInterface
+    {
+        void root();
+        void copyFilesFromDevice(string storingPathInComputer, Dictionary<string, string> appsPathInDevice);
+        void setAppCheckBoxes();
+    }
+}
