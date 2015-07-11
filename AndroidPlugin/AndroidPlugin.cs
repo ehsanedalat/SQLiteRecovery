f7c4c79600db7ebac8e91fa751146a81f2091530
ehsan-is-everything <ehsan.e.71@gmail.com>
2015-07-11 13:57:16 +0430

    references added. interface was implemented!

---
+++ AndroidPlugin/AndroidPlugin.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DevicePluginInterface;
+
+namespace AndroidPlugin
+{
+    public class AndroidPlugin : DeviceRecoveryPluginInterface
+    {
+        #region overide interface methodes
+        
+        public void root()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void copyFilesFromDevice(string storingPathInComputer, Dictionary<string, string> appsPathInDevice)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setAppCheckBoxes()
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+    }
+}
