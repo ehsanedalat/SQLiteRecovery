18bd6d250cebe3e6d330860cfb33b0cca8a82719
ehsan-is-everything <ehsan.e.71@gmail.com>
2015-07-11 11:23:35 +0430

    plugin interface and plugin service class was created!

---
+++ SQLiteRecovery/DeviceRecoveryPlugin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SQLiteRecovery
+{
+    interface DeviceRecoveryPlugin
+    {
+        void root();
+        void copyFilesFromDevice(string storingPathInComputer, Dictionary<string,string> appsPathInDevice);
+        void setAppCheckBoxes();
+    }
+}
